f4134c33807627bd220772adafb5525e6ef5f117
dice <dicenull.dev@gmail.com>
2019-02-14 14:17:48 +0900

    3単語ずつのブロックに分割するメソッドを実装

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NMeCab;
+using System.IO;
 
 namespace TwitterDiceAI
 {
@@ -10,6 +12,16 @@ namespace TwitterDiceAI
 	{
 		static void Main(string[] args)
 		{
+			var mecab = MeCabTagger.Create(new MeCabParam
+			{
+				DicDir = Path.Combine(AppContext.BaseDirectory, "ipadic"),
+			});
+
+			var firstNode = mecab.ParseToNode("これはペンですか。");
+			foreach(var block in firstNode.ToEnumerable().ToBlocks())
+			{
+				Console.WriteLine($"{block.Key} {block.Value} {block.Next} ");
+			}
 		}
 	}
 }
